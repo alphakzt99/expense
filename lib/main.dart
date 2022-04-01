811087b24237e69b6c96614c692cf3b3da1a602e
Kaung Zaw Thant <kaung.thant@aiesec.net>
2022-04-01 21:37:58 +0630

    first

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Expense());
+
+class Expense extends StatefulWidget {
+  Expense({Key? key}) : super(key: key);
+
+  @override
+  State<Expense> createState() => _ExpenseState();
+}
+
+class _ExpenseState extends State<Expense> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
