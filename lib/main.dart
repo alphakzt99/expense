73580ec56731b50a48c8e143fddbc5a4035cac47
Kaung Zaw Thant <kaung.thant@aiesec.net>
2022-04-02 15:54:00 +0630

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'transcations.dart';
 
 void main() => runApp(Expense());
 
@@ -10,8 +11,54 @@ class Expense extends StatefulWidget {
 }
 
 class _ExpenseState extends State<Expense> {
+  final List<Transcations> transcations = [
+    Transcations(
+        id: "01", title: "New Phone", amount: 69.99, date: DateTime.now()),
+    Transcations(
+        id: "02", title: "New Shoes", amount: 19.99, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+          ),
+          body: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: transcations.map((tx) {
+                return Card(
+                    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                    elevation: 10,
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            margin: EdgeInsets.all(10),
+                            decoration: BoxDecoration(border: Border.all(width: 2)),
+                            child: Text(
+                              tx.amount.toString(),
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(tx.title, style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                              Text(tx.date.toString(),style: TextStyle(color: Colors.grey))
+                            ],
+                          )
+                        ],
+                      ),
+                    ));
+              }).toList(),
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/transcations.dart
@@ -0,0 +1,11 @@
+class Transcations {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  Transcations(
+      {required this.id,
+      required this.title,
+      required this.amount,
+      required this.date});
+}
