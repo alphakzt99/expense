6eb3c46a78eb8b5e24f7ea8e98b2677f609ed713
Kaung Zaw Thant <kaung.thant@aiesec.net>
2022-04-07 22:44:35 +0630

    bottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:expense/widget/transcation_list.dart';
+import 'package:expense/widget/user_transcations.dart';
 
+import 'package:expense/models/transcations.dart';
 
-void main() => runApp(Expense());
+void main() => runApp(Home());
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expense(),
+    );
+  }
+}
 
 class Expense extends StatefulWidget {
   Expense({Key? key}) : super(key: key);
@@ -12,22 +26,54 @@ class Expense extends StatefulWidget {
 }
 
 class _ExpenseState extends State<Expense> {
-  
+  List<Transcations> usertranscations = [
+    Transcations(
+        id: "01", title: "New shoe", amount: 69.99, date: DateTime.now()),
+    Transcations(
+        id: "02", title: "New phone", amount: 123.99, date: DateTime.now())
+  ];
+  void _addTranscation(String txtitle, double txamount) {
+    final newTx = Transcations(
+        id: DateTime.now().toString(),
+        title: txtitle,
+        amount: txamount,
+        date: DateTime.now());
+    setState(() {
+      usertranscations.add(newTx);
+    });
+  }
+
+  void _startNewTranscation(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (bctx) {
+          return GestureDetector(
+            onTap: (){},
+            child: UserTranscations(_addTranscation),
+            behavior: HitTestBehavior.opaque,);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.purple,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startNewTranscation(context),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(children: [
+          Container(),
+          TranscationList(
+            usertranscations: usertranscations,
           ),
-          body: 
-            Column(children: [
-              Container(),
-              TranscationList(),
-              
-            ]),
-          ),
-        );
+        ]),
+      ),
+    );
   }
 }
--- lib/widget/transcation_list.dart
+++ lib/widget/transcation_list.dart
@@ -1,39 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:expense/main.dart';
 import 'package:expense/models/transcations.dart';
 import 'package:expense/widget/new_transcations.dart';
 import 'package:expense/widget/user_transcations.dart';
 
 class TranscationList extends StatefulWidget {
-  TranscationList({Key? key}) : super(key: key);
+  List usertranscations = [];
+  TranscationList({Key? key,required this.usertranscations}) : super(key: key);
 
   @override
   State<TranscationList> createState() => _TranscationListState();
 }
 
 class _TranscationListState extends State<TranscationList> {
-   List<Transcations> usertranscations = [
-    Transcations(
-        id: "01", title: "New shoe", amount: 69.99, date: DateTime.now()),
-    Transcations(
-        id: "02", title: "New phone", amount: 123.99, date: DateTime.now())
-  ];
-  void _addTranscation(String txtitle, double txamount) {
-    final newTx = Transcations(
-        id: DateTime.now().toString(),
-        title: txtitle,
-        amount: txamount,
-        date: DateTime.now());
-    setState(() {
-      usertranscations.add(newTx);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        UserTranscations(_addTranscation),
-        NewTranscations(usertranscations),
+        NewTranscations(widget.usertranscations),
       ],
     );
   }
